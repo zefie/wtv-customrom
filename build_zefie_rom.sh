45ce7a46bcd4ece9f608f47fd17df0df915b4913
zefie <zefie@zefie.net>
2024-05-18 11:46:32 -0400

    initial commit

---
+++ build_zefie_rom.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd "$(realpath "$(dirname "${0}")")" || exit 1
+
+ZWNIBUILD=1235
+ZVERSION="zefie-v2"
+ZROMTYPE="bf0app"
+
+ZSRC="src"
+ZWORK="workdir"
+ZROM="htv-${ZVERSION}.o"
+ZDEST="parts"
+
+ZINFOJS="${ZWORK}/tmp-romfs/ROM/JS/BuildInfo.js"
+ZDATE=$(date +"%Y-%m-%d %H:%M:%S %Z") 
+
+
+
+# Copy SRC to WORK
+if [ -d "${ZWORK}" ]; then
+	rm -r "${ZWORK}"
+fi
+
+cp -R "${ZSRC}" "${ZWORK}"
+
+# Patch Vars in Dynamic JS
+sed -i "s|!TEMPLATE_DATE!|${ZDATE}|" "${ZINFOJS}"
+sed -i "s|!TEMPLATE_VERS!|${ZVERSION}|" "${ZINFOJS}"
+sed -i "s|!TEMPLATE_ROM!|${ZROMTYPE}|" "${ZINFOJS}"
+
+# Build ROM
+rommy --fixcs "${ZWORK}" "${ZROM}"
+# --disable-lzss-compression
+
+# Convert to Parts
+if [ "${1}" == "parts" ]; then
+	if [ -d "${ZDEST}" ]; then
+		rm -r "${ZDEST}"
+	fi
+	rommy --rom-blocks --rom-block-prefix "${ZROMTYPE}-" --rom-block-message "HackTV ${ZWNIBUILD}-${ZVERSION} Pt {index}" "${ZROM}" "${ZDEST}"
+fi
---
+++ src/tmp-romfs/ROM/JS/htv-header.js
@@ -0,0 +1,161 @@
+String.prototype.replace = function(o, n) {
+	return this.split(o).join(n);
+}
+
+var d = document;
+
+function getThemeName(th) {	
+	return z_th[parseInt(th)]
+}
+
+function getThemeColor(th, type) {
+	// light
+	bgclr = '4c5a67'
+	tclr = 'cbcbcb'
+	vclr = 'dddddd'
+	lclr = 'dddddd'
+
+	switch (th) {
+		case 1:
+			// dark
+			bgclr = '191919'
+			tclr = 'aaaaaa'
+			break
+		case 2:
+			// purple
+			bgclr = '4a2766'
+			lclr = 'aaaaaa'
+			break
+		case 3:
+			// blue
+			bgclr = '003355'
+			break
+	}
+
+	switch (type) {
+		case 'bg':
+			return bgclr
+		case 't':
+			return tclr
+		case 'v':
+			return vclr
+		case 'l':
+			return lclr
+	}
+}
+function head(th, fs, bgm, lp, msg) {
+	switch (fs) {
+		case 'small':
+			fsn = 7
+			break
+		case 'large':
+			fsn = 4
+			break
+		default:
+			fs = 'medium'
+			fsn = 5
+			break
+	}
+	
+	bgclr = getThemeColor(th, 'bg')
+	tclr = getThemeColor(th, 't')
+	vclr = getThemeColor(th, 'v')
+	lclr = getThemeColor(th, 'l')
+
+	if (msg) {
+		d.write('<title>'+msg+'</title>')
+	}
+	d.write('<body background=file://rom/Images/Themes/Pattern.gif text='+tclr+' bgcolor='+bgclr+' vlink='+vclr+' link='+lclr+' hspace=0 vspace=0 fontsize='+fs+'>')
+	if (bgm) {
+		if (bgm.indexOf('.') < 0) {
+			bgm += '.mid'
+		}
+		d.write('<bgsound src=file://rom/Cache/Music/'+bgm+' autostart=true')
+		if (!lp) {
+			d.write('>')
+		} else {
+			if (lp == -1) {
+				lp = 9999;
+			}
+			d.write(' loop='+lp+'>')
+		}
+	}
+	
+	if (!msg) {
+		msg = ""
+	}
+	
+	d.write('<table cellspacing=0 cellpadding=0>')
+    d.write('<tr><td>')
+    tab();
+    d.write('<spacer type=block width=11 height=11><br>')
+    d.write('<spacer type=block width=10 height=1><a href="javascript:goHTV()"><img src=file://ROM/Cache/WebTVLogoJewel.gif width=90 height=69></a>')
+	tab(msg);
+    d.write('</td></tr></table>')
+}	
+
+function tab(msg) {
+	msg = msg.replace(' ','&nbsp;')
+	if (msg) {
+		d.write('<td width=100% height=80 valign=top background=file://ROM/Images/Themes/ShadowLogo.gif novtilebg><td abswidth=460 height=54 valign=top background=file://ROM/Images/Themes/ShadowLogo.gif align=right novtilebg><spacer height=32 type=block><b><shadow><blackface><font color=cbcbcb>'+msg+' &nbsp; </font></blackface></shadow></b>')
+	} else {
+		d.write('<td width=100% height=80 valign=top align=left background="file://ROM/Images/Themes/ShadowLogo.gif" novtilebg>')
+	}
+}
+
+function ci(th) {
+	bgclr = getThemeColor(th, 'bg')
+	tclr = getThemeColor(th, 't')
+	d.write('&nbsp;&nbsp;<textarea rows=4 size=46 id=msg name=msg border=0 text='+tclr+' bgcolor='+bgclr+' value="" nohighlight noselect></textarea>')
+}
+
+function as(th, bg, h, w, g, b, lc, rc, lo, ro, s) {
+	
+	if (s) {
+		if (!lc) { lc = getThemeColor(th, 't') }
+		if (!rc) { rc = getThemeColor(th, 'bg') }
+	} else {
+		if (!lc) { lc = getThemeColor(th, 'bg') }
+		if (!rc) { rc = getThemeColor(th, 't') }
+	}
+	if (th == 1) { bgclr = '333333' }
+	if (!bg) { bg = '191919' }
+	if (!h) { h = 32 }
+	if (!w) { w = 320 }
+	if (!g) { g = 0 }
+	if (!lo) { lo = 0 }
+	if (!ro) { ro = 0 }
+	if (!b) { b = 1 }
+	d.write('<audioscope bgcolor='+bg+' height='+h+' width='+w+' gain='+g+' leftcolor='+tclr+' rightcolor='+bgclr+' leftoffset='+lo+' rightoffset='+ro+' border='+b+'>')
+}
+
+function go(u) {
+	location.href = u
+	return true
+}
+
+function goHTV() {
+	return go('client:gotoadvancedsetup')
+}
+
+function butt(th, v, n, w, t, x) {
+	tclr = getThemeColor(th, 't')
+	d.write('<font color='+tclr+'>')
+	if (!t) t = 'submit';
+	d.write('<input type='+t+' value="'+v+'"')
+	if (n) d.write(' name='+n)
+	if (w) d.write(' width='+w)
+	if (x) d.write(' '+x)
+	if (th == 1) {
+		d.write(' usestyle borderimage=file://ROM/Borders/ButtonBorder4.bif')
+	} else if (th == 2 || th == 3) {
+		d.write(' usestyle borderimage=file://ROM/Borders/ButtonBorderTh'+th+'.bif')
+	} 
+	d.write('></font>');
+}
+
+function dial() {
+	go("client:redialphone")
+	go("client:logoshown")
+}
+	
