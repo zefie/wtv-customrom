a23f9330af0cded5940c8fff69728a9a38eb0345
zefie <zefie@zefie.net>
2024-05-27 17:07:44 -0400

    update DiskFlash n.js

--- src/level0-autodisk/Browser/DiskFlash/n.js
+++ src/level0-autodisk/Browser/DiskFlash/n.js
@@ -1,12 +1,11 @@
-var z_nv=null;
-
+z_nv=null;
 z_th=new Array()
 	z_th[0]='HackTV Light'
 	z_th[1]='WebTV Dark'
 	z_th[2]='Avegee Red'
 	z_th[3]='Pumpkin Orange'
 	z_th[4]='WinXP Tan'
-	z_th[5]='Forest Green'
+	z_th[5]='Ryder Green'
 	z_th[6]='SKCro Blue'
 	z_th[7]='Win95 Teal'
 	z_th[8]='zefie Purple'
@@ -52,15 +51,12 @@ var z_def=new Array()
 	z_def[1]=7//bgm
 
 chars="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@-"//64 possible different values
-
 function gTN(th){return z_th[parseInt(th)]}
-
 function gB(nv,off){
 	b=nv.charAt(off)
 	if(b){return chars.indexOf(b)}
 	else{return -1}
 }
-
 function sB(nv,off,dat){
 	prefix=''
 	if(off > 0){prefix=nv.substring(0,off)}
@@ -69,34 +65,28 @@ function sB(nv,off,dat){
 	z_url='client:ConfirmBYOISPSetup?BYOISPProviderName='+prefix+dat+nv.substring(off+1)
 	go(z_url)
 }
-
 function gV(nv,off){
 	z_len=0
 	switch(off){
-		case 0:
-			z_len=z_th.length
-			break
-		case 1:
-			z_len=z_bgm.length
-			break
+		case 0:z_len=z_th.length
+		break
+		case 1:z_len=z_bgm.length
+		break
 	}
 	z_val=gB(nv,off)
-	if(z_val < 0 || z_val >= z_len){return parseInt(z_def[off])}
+	if(z_val<0||z_val>=z_len){return parseInt(z_def[off])}
 	return parseInt(z_val)
 }
-
-function gBGM(nv,n,rn) {		
+function gBGM(nv,n,rn){
 		if(n){p=n}
 		else{p=gV(nv,1)}
-		
 		if(rn){return p}
 		f=z_bgm[p];
-		if(f.indexOf('.') < 0){f+='.mid'}
+		if(f.indexOf('.')<0){f+='.mid'}
 		return 'file://rom/Cache/Music/'+f
 }
-
 function pp(){
-	d.write('<form name=z><input type=hidden name=h value="&pname;"></form>')
+	d.write('<form name=z><input type=hidden name=h value=&pname;></form>')
 	z_nv=d.z.h.value
 	return parseInt(gV(z_nv,0))//theme
 }
