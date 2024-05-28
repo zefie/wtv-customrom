99ddb080243046b0df988ec185f3b03e7aa2a947
SK <55817746+SKCro@users.noreply.github.com>
2024-05-27 22:34:53 -0400

    fart compression

--- src/tmp-romfs/ROM/JS/farts.js
+++ src/tmp-romfs/ROM/JS/farts.js
@@ -1,5 +1,4 @@
 function dF2(){dF(5);}//m minus number of non farts
-
 function dF(m){
 	if(!m){m=7}//2 higher than last number
 	r=Math.floor(Math.random()*m)
@@ -8,11 +7,8 @@ function dF(m){
 	else if(r==2){u="file://ROM/Cache/Audio/fart3.mp3"}
 	else if(r==3){u="file://ROM/Cache/Audio/fart4.mp3"}
 	else if(r==4){u="file://ROM/Cache/Audio/fart6.mp3"}
-	else if(r==5){
-		u="file://ROM/Cache/Audio/engaged.mp3"
-		setTimeout(dF2, 1600)
-	}else{
-		u="file://ROM/Cache/Audio/bum.mp3"
-		setTimeout(dF2, 1500)
-	}location=u
-}
+	else if(r==5){u="file://ROM/Cache/Audio/engaged.mp3"
+	setTimeout(dF2,1600)
+	}else{u="file://ROM/Cache/Audio/bum.mp3"
+	setTimeout(dF2,1500)
+	}location=u}
